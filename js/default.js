72ae117914e5185a37553de26e9adfacd37c2bf8
紅月九音 <teiruzuof.15@gmail.com>
2017-06-19 17:05:56 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,2 +1,15 @@
 var number = Math.floor(Math.random()*3);
-var answer = parseInt(window.
+var answer = parseInt(window.prompt('数あてゲーム！0～3を入力してください！'));
+
+var message;
+if(answer === number){
+  message = 'あたり！Σ(･ω･ﾉ)ﾉ！';
+}else if (answer < number){
+  message = 'ざんね～ん。もっと大きいよ！( *´艸｀)';
+}else if (number < answer){
+  message = 'ざんね～ん。もっと小さいよ！( *´艸｀)';
+}else{
+  message = '0~3の数字を入力してくださいな( ..)φ';
+}
+
+document.getElementById('choice').textContent = message;
